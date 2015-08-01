1d94208d0df42e8e8f539a07b547ddf1d8683b56
bcolb <brice.tech@gmail.com>
2015-08-01 09:40:15 -0700

    Added customizable script afterLoad.js

---
+++ afterLoad.js
@@ -0,0 +1,8 @@
+/**
+ * ADD YOUR CODE HERE!
+ * This file loads after tools, so if you want to automate commands,
+ * like starting your miner, setting the mining benefactor,
+ * or printing out a report, you can do it here!
+ */
+
+tools.ether.checkCoinbase() // Ensures that coinbase/mining benefactor is set
